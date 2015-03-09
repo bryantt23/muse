aae95d00e08e75feac0ed0a113d37fbb5d797818
bryant tee <bryantt23@gmail.com>
2015-03-08 23:39:00 -0700

    set up paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
-  
+                                                                             
   belongs_to :user
+  has_attached_file :image, :styles => { :medium => "700x550>", :small =>"400x350" }
+  # has_attached_file :image, styles: { medium: "700x550", small: "400x350" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    
 end
